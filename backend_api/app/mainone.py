d2b19467095a5b21d7c96c6ce195490a15662a0d
Nabin <mario@Mario-Mbp.local>
2026-05-09 18:49:11 -0700

    creating endpoint starting point wisth echo

---
+++ backend_api/app/mainone.py
@@ -0,0 +1,32 @@
+from pyexpat.errors import messages
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class EchoRequest(BaseModel):
+    message: str
+
+
+@app.get("/")
+def home():
+    return {"message": "Backend is running"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.post("/echo")
+def echo(data: EchoRequest):
+    messages
+    print("original_message"  data.message,
+        "uppercase_message": data.message.upper(),
+        "length": len(data.message)
+    return {
+        "original_message": data.message,
+        "uppercase_message": data.message.upper(),
+        "length": len(data.message)
+    }
